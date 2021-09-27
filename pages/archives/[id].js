6d90a2f972672a4d494469c5641807cc705c3d6d
Afif Akromi <massachusetts48@gmail.com>
2021-09-27 20:34:34 +0700

    minor changes

--- pages/archives/[id].js
+++ pages/archives/[id].js
@@ -3,13 +3,62 @@ import Layout from "../../components/commons/Layout";
 import React from "react";
 import "react-dropdown/style.css";
 import Image from "next/image";
-import { gff_archive } from "../../const";
+// import { gff_archive } from "../../const";
 import { useRouter } from "next/router";
 
 const Details = () => {
   const router = useRouter();
   const { id } = router.query;
-
+  const gff_archive = [
+    {
+      link: "/archives/1",
+      key: "GFF 2018",
+      title: "Ganffest 2018: Jendela",
+      content:
+        "Tema Jendela diangkatan dengan harapan Ganffest 2018 dapat menjalankan fungsinya sebagai sebuah jendela. Jendela dapat digunakan sebagai media untuk melihat dari suatu tempat ke sebuah “cakrawala” lainnya. Bandung diumpamakan sebagai sebuah rumah yang di dalamnya terdapat film-film Bandung dan lingkungan luar Bandung diumpamakan sebagai lingkungan luar tersebut, dan Ganffest 2018 diumpamakan sebagai jendela dari rumah tersebut yang dapat menghubungkan orang-orang dari luar rumah (Bandung) dapat bisa melihat film-film dalam rumah tersebut, dan sebaliknya.",
+      fotoUrl: "/img/gff2018.jpg",
+    },
+    {
+      link: "/archives/2",
+      key: "GFF 2016",
+      title: "Ganffest 2018: Jendela",
+      content:
+        "Tema Jendela diangkatan dengan harapan Ganffest 2018 dapat menjalankan fungsinya sebagai sebuah jendela. Jendela dapat digunakan sebagai media untuk melihat dari suatu tempat ke sebuah “cakrawala” lainnya. Bandung diumpamakan sebagai sebuah rumah yang di dalamnya terdapat film-film Bandung dan lingkungan luar Bandung diumpamakan sebagai lingkungan luar tersebut, dan Ganffest 2018 diumpamakan sebagai jendela dari rumah tersebut yang dapat menghubungkan orang-orang dari luar rumah (Bandung) dapat bisa melihat film-film dalam rumah tersebut, dan sebaliknya.",
+      fotoUrl: "/img/gff2018.jpg",
+    },
+    {
+      link: "/archives/3",
+      key: "GFF 2014",
+      title: "Ganffest 2018: Jendela",
+      content:
+        "Tema Jendela diangkatan dengan harapan Ganffest 2018 dapat menjalankan fungsinya sebagai sebuah jendela. Jendela dapat digunakan sebagai media untuk melihat dari suatu tempat ke sebuah “cakrawala” lainnya. Bandung diumpamakan sebagai sebuah rumah yang di dalamnya terdapat film-film Bandung dan lingkungan luar Bandung diumpamakan sebagai lingkungan luar tersebut, dan Ganffest 2018 diumpamakan sebagai jendela dari rumah tersebut yang dapat menghubungkan orang-orang dari luar rumah (Bandung) dapat bisa melihat film-film dalam rumah tersebut, dan sebaliknya.",
+      fotoUrl: "/img/gff2018.jpg",
+    },
+    {
+      link: "/archives/4",
+      key: "GFF 2012",
+      title: "Ganffest 2018: Jendela",
+      content:
+        "Tema Jendela diangkatan dengan harapan Ganffest 2018 dapat menjalankan fungsinya sebagai sebuah jendela. Jendela dapat digunakan sebagai media untuk melihat dari suatu tempat ke sebuah “cakrawala” lainnya. Bandung diumpamakan sebagai sebuah rumah yang di dalamnya terdapat film-film Bandung dan lingkungan luar Bandung diumpamakan sebagai lingkungan luar tersebut, dan Ganffest 2018 diumpamakan sebagai jendela dari rumah tersebut yang dapat menghubungkan orang-orang dari luar rumah (Bandung) dapat bisa melihat film-film dalam rumah tersebut, dan sebaliknya.",
+      fotoUrl: "/img/gff2018.jpg",
+    },
+    {
+      link: "/archives/5",
+      key: "GFF 2010",
+      title: "Ganffest 2018: Jendela",
+      content:
+        "Tema Jendela diangkatan dengan harapan Ganffest 2018 dapat menjalankan fungsinya sebagai sebuah jendela. Jendela dapat digunakan sebagai media untuk melihat dari suatu tempat ke sebuah “cakrawala” lainnya. Bandung diumpamakan sebagai sebuah rumah yang di dalamnya terdapat film-film Bandung dan lingkungan luar Bandung diumpamakan sebagai lingkungan luar tersebut, dan Ganffest 2018 diumpamakan sebagai jendela dari rumah tersebut yang dapat menghubungkan orang-orang dari luar rumah (Bandung) dapat bisa melihat film-film dalam rumah tersebut, dan sebaliknya.",
+      fotoUrl: "/img/gff2018.jpg",
+    },
+    {
+      link: "/archives/6",
+      key: "GFF 2008",
+      title: "Ganffest 2018: Jendela",
+      content:
+        "Tema Jendela diangkatan dengan harapan Ganffest 2018 dapat menjalankan fungsinya sebagai sebuah jendela. Jendela dapat digunakan sebagai media untuk melihat dari suatu tempat ke sebuah “cakrawala” lainnya. Bandung diumpamakan sebagai sebuah rumah yang di dalamnya terdapat film-film Bandung dan lingkungan luar Bandung diumpamakan sebagai lingkungan luar tersebut, dan Ganffest 2018 diumpamakan sebagai jendela dari rumah tersebut yang dapat menghubungkan orang-orang dari luar rumah (Bandung) dapat bisa melihat film-film dalam rumah tersebut, dan sebaliknya.",
+      fotoUrl: "/img/gff2018.jpg",
+    },
+  ];
   return (
     <Layout title={gff_archive[id - 1].key}>
       <Navbar />
