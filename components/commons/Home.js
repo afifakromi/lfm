17922a0a22d72e9873fa5ebe1eec5c5e39dd062e
Afif Akromi <massachusetts48@gmail.com>
2021-09-27 20:05:32 +0700

    add video

--- components/commons/Home.js
+++ components/commons/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import Login from "../auth/Login";
 import Register from "../auth/Register";
+import ReactPlayer from "react-player";
 
 // Untuk hide/show Login Register
 import { useOpen } from "../../context/ToggleContext";
@@ -12,6 +13,16 @@ const Home = () => {
     <div className="relative w-full h-screen bg-primary">
       <Login open={optionsOpen[0].loginOpen} />
       <Register open={optionsOpen[0].registerOpen} />
+      <div className="w-full">
+        <ReactPlayer
+          url="/video-landing.mp4"
+          playing={true}
+          loop={true}
+          light={false}
+          width="100%"
+          height="100%"
+        />
+      </div>
     </div>
   );
 };
