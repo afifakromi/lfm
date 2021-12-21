86aa29afe842173f19f89a245b895508cfef276c
Afif Akromi <massachusetts48@gmail.com>
2021-12-22 00:17:33 +0700

    edit submission

--- authentication/server-config.js
+++ authentication/server-config.js
@@ -2,6 +2,8 @@ export const serverURL = "https://backend.ganffest2022.xyz/api";
 // export const serverURL = "http://bekend.ganffest2022.xyz/api";
 // export const serverURL = "http://127.0.0.1:8000/api";
 
+// export const serverURL = "http://127.0.0.1:8000/api";
+
 export const routes = {
   register: "/register",
   login: "/login",
