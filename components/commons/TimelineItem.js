b8dab9535b5282e0959ff7aafc37a29cab8a437a
Afif Akromi <massachusetts48@gmail.com>
2022-02-03 21:05:32 +0700

    fix timeline

--- components/commons/TimelineItem.js
+++ components/commons/TimelineItem.js
@@ -7,6 +7,7 @@ const TimelineItem = ({
   isTop,
   rounded,
   content,
+  isBottom,
 }) => {
   const [hide, setHide] = useState(false);
 
@@ -14,7 +15,7 @@ const TimelineItem = ({
     <div className="flex flex-row space-x-8 items-center cursor-pointer">
       <div
         className={`w-16 h-32 ${
-          !isTop ? "border-b-4 border-white" : null
+          !isBottom ? "border-b-4 border-white" : null
         } relative ${rounded}`}
         style={{ backgroundColor: bgColor }}
         onMouseOver={() => setHide(true)}
