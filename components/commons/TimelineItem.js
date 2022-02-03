4d171d74211f2905668dedf39b38d38b4d44a692
Afif Akromi <massachusetts48@gmail.com>
2022-02-04 01:29:49 +0700

    not fix

--- components/commons/TimelineItem.js
+++ components/commons/TimelineItem.js
@@ -22,7 +22,9 @@ const TimelineItem = ({
         onMouseOver={() => setHide(true)}
         onMouseLeave={() => setHide(false)}
       >
-        <p className={`text-xl w-24 absolute top-1/2 ${className}`}>{date}</p>
+        <p className={`text-xl w-24 absolute top-1/2 text-white ${className}`}>
+          {date}
+        </p>
         <div
           className={` bg-white rounded-xl px-10 py-4 w-96 z-10 text-black ${
             hide ? "absolute" : "hidden"
