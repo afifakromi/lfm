f961b9907ebda87a2b86cc992a8450b6c69d6ca6
bintangpananjung <68179028+bintangpananjung@users.noreply.github.com>
2022-02-13 23:24:36 +0700

    Revert "Revert "Merge branch 'forum'""
    This reverts commit f6e2b507acb54b34871f5429cbc7f38cf9012017.

---
+++ components/forum/content/ForumContentCard.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import { forum_db } from "../../../firebase/forumConfig";
+import { createdAt } from "../../../helper/time";
+import firebase from "firebase/app";
+import LikeButton from "../commons/LikeButton";
+import DislikeButton from "../commons/DislikeButton";
+import ForumContentModal from "./ForumContentModal";
+import ForumMedia from "./ForumMedia";
+
+const ForumContentCard = ({ postdata, interacted }) => {
+  const [togglemodal, settogglemodal] = useState(false);
+  const [liked, setliked] = useState();
+  useEffect(() => {
+    if (interacted) {
+      setliked(interacted.data().state);
+    }
+  }, [interacted]);
+
+  return (
+    <div className="flex p-5 bg-white shadow-lg rounded-xl mb-12">
+      <div className="flex flex-col mr-9 items-center">
+        <LikeButton sourcedata={postdata} liked={liked} />
+        <p className="my-3 text-center">
+          {postdata.data().liked - postdata.data().disliked}
+        </p>
+        <DislikeButton sourcedata={postdata} liked={liked} />
+      </div>
+      <div className="flex flex-col w-full">
+        <p className="text-gray-600 text-xl font-bold mb-1">
+          {postdata.data().topic}
+        </p>
+        <p className="text-gray-500 text-sm font-gilroy mt-3">
+          {postdata.data().description}
+        </p>
+        <ForumMedia media={postdata} />
+
+        <div className="flex pt-2 border-t-2 border-gray-200 items-center text-sm">
+          <img src="/img/pp.png" alt="" />
+          <p className="font-gilroy ml-2">Posted by</p>
+          <p className="font-gilroy ml-4 text-customBlueForum">
+            {postdata.data().username}
+          </p>
+          <p className="font-gilroy ml-10 ">
+            {createdAt(postdata.data().created_at.seconds)}
+          </p>
+          <button
+            className="ml-auto"
+            onClick={e => {
+              e.preventDefault();
+              settogglemodal(true);
+            }}
+          >
+            <img src="/img/commentgrey.png" alt="" />
+          </button>
+          <p className="ml-2 text-gray-500 mr-5">{postdata.data().comments}</p>
+          {/* <img src="/img/sharegrey.png" alt="" className="ml-3 mr-8" /> */}
+        </div>
+      </div>
+      <ForumContentModal
+        postdata={postdata}
+        togglemodal={togglemodal}
+        settogglemodal={settogglemodal}
+        liked={liked}
+      />
+    </div>
+  );
+};
+
+export default ForumContentCard;
---
+++ components/forum/content/ForumContentPagination.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+
+const ForumContentPagination = ({ page, maxPage, setpage }) => {
+  const [isEndSection, setisEndSection] = useState(false);
+  useEffect(() => {
+    if (!maxPage) return;
+    if (Math.floor((maxPage - 1) / 3) - Math.floor((page - 1) / 3) === 0) {
+      setisEndSection(true);
+    } else {
+      setisEndSection(false);
+    }
+  }, [page, maxPage]);
+
+  const loadButtonPage = () => {
+    var data = [];
+    var section = Math.floor((page - 1) / 3);
+    for (let i = section * 3; i < (section + 1) * 3; i++) {
+      if (i === page - 1)
+        data.push(
+          <p
+            className="text-white text-center bg-customBlueForum rounded-lg w-8 py-1 mr-2"
+            key={i}
+          >
+            {i + 1}
+          </p>
+        );
+      else {
+        data.push(
+          <button
+            className="text-black rounded-lg w-8 py-1 mr-2 hover:bg-customBlueForum hover:text-white"
+            key={i}
+            onClick={e => {
+              setpage(i + 1);
+            }}
+          >
+            {i + 1}
+          </button>
+        );
+      }
+    }
+    return data;
+  };
+  return (
+    <div className="flex items-center justify-center mt-20">
+      {page === 1 ? (
+        ""
+      ) : (
+        <button
+          className="w-24 py-1 border-blue-500 border-2 rounded-xl text-customBlueForum font-qanelas text-sm mr-3 hover:bg-customBlueForum hover:text-white"
+          onClick={e => {
+            setpage(page - 1);
+          }}
+        >
+          PREVIOUS
+        </button>
+      )}
+      {loadButtonPage()}
+      {isEndSection ? "" : <p className="mr-2 text-black w-8 py-1">...</p>}
+      {maxPage && page === maxPage ? (
+        ""
+      ) : (
+        <button
+          className="w-24 py-1 border-blue-500 border-2 rounded-xl text-customBlueForum font-qanelas text-sm hover:bg-customBlueForum hover:text-white"
+          onClick={e => {
+            setpage(page + 1);
+          }}
+        >
+          NEXT
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default ForumContentPagination;
